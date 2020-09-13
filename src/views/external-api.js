7f52b86bf9e883dcec7f9a7e1b6f09db22358b53
mikecadima <cadimamichael@gmail.com>
2020-09-12 20:15:52 -0400

    first commit

---
+++ src/views/external-api.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+// function external-api() {
+//     return (
+//         <div>
+//             <h1> Testing </h1>
+//         </div>
+//     )
+// }
+
+// export default external-api
---
+++ src/views/welcomepage.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+import { Hero, Content } from "../components";
+
+const welcomepage = () => (
+  <Fragment>
+    <Hero />
+    <h1>
+        The welcome page
+    </h1>
+    <hr />
+    <Content />
+  </Fragment>
+);
+
+export default welcomepage;
